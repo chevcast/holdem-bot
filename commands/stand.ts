deacfa21251692582decf527e127a751a9e26ebf
Chev <chev@chevtek.io>
2020-09-09 15:20:54 -0600

    Relay new owner, leavers, and destruction messages to all players.

--- commands/stand.ts
+++ commands/stand.ts
@@ -62,15 +62,25 @@ export async function handler ({ discord, user }) {
     if (!account) throw new Error("Cannot find account. Unable to return player stack.");
     playersThatStood.forEach(({ stackSize }) => account.bankroll += stackSize);
     await Promise.all([account.saveToDb(), table.render()]);
+    const leftUser = message.client.users.cache.get(userId)!;
     if (message.author.id !== userId) {
-      await Promise.all([
-        message.reply(`<@${userId}> has been removed from the table.`),
-        table.channel.send(`<@${userId}> has been removed from the table.`)
+      await Promise.all<any>([
+        table.channel.send(`<@${userId}> has been removed from the table.`),
+        ...table.players.filter(player => player !== null).map(async player => {
+          const user = message.client.users.cache.get(player!.id);
+          const channel = user!.dmChannel || await user!.createDM();
+          await channel.send(`${leftUser.username} has been removed from the table.`);
+        })
       ]);
     } else {
-      await Promise.all([
+      await Promise.all<any>([
         message.reply("You have left your active Hold'em table."),
-        table.channel.send(`<@${userId}> has left the table.`)
+        table.channel.send(`<@${userId}> has left the table.`),
+        ...table.players.filter(player => player !== null && player.id !== userId).map(async player => {
+          const user = message.client.users.cache.get(player!.id);
+          const channel = user!.dmChannel || await user!.createDM();
+          await channel.send(`${leftUser.username} has left from the table.`);
+        })
       ]);
     }
     const players = table.players.filter(player => player !== null);
@@ -78,11 +88,16 @@ export async function handler ({ discord, user }) {
       if (table.creatorId === userId) {
         const [newOwner] = players;
         table.creatorId = newOwner!.id;
-        const user = message.client.users.cache.get(table.creatorId);
-        const channel = user!.dmChannel || await user!.createDM();
-        await Promise.all([
+        const ownerUser = message.client.users.cache.get(table.creatorId)!;
+        const channel = ownerUser!.dmChannel || await ownerUser!.createDM();
+        await Promise.all<any>([
           channel.send(`<@${table.creatorId}>, you are now the table owner.`),
-          table.channel.send(`<@${table.creatorId}> is the new table owner.`)
+          table.channel.send(`<@${table.creatorId}> is the new table owner.`),
+          ...table.players.filter(player => player !== null && player.id !== table!.creatorId).map(async player => {
+            const user = message.client.users.cache.get(player!.id);
+            const channel = user!.dmChannel || await user!.createDM();
+            await channel.send(`${ownerUser.username} is the new table owner.`);
+          })
         ]);
       }
       await table.saveToDb();
