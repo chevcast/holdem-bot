6fe5a6f1b1d1cddb9dea3f81235f52d5011354fa
Chev <chev@chevtek.io>
2020-10-19 16:24:05 -0600

    Update terms command to point to an external markdown file.

--- commands/terms.ts
+++ commands/terms.ts
@@ -10,6 +10,7 @@ export const description = "Show a list of poker terms.";
 
 export async function handler ({ discord }) {
   const message = discord.message as Message;
-  const pokerTerms = (await readFile("./poker-terms.md")).toString();
-  await message.channel.send(pokerTerms.split("\n").map(line => line.trimEnd()).join("\n"), { split: true });
+  // const pokerTerms = (await readFile("./poker-terms.md")).toString();
+  // await message.channel.send(pokerTerms.split("\n").map(line => line.trimEnd()).join("\n"), { split: true });
+  await message.reply("Please visit https://gist.github.com/chevtek/69bb18781d96aa930d8d1e45c28dd5ba for a glossary of useful poker terms :)");
 }
